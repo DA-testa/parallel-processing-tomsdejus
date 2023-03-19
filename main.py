7eb7d2f7fc97bdc44c32ba78ce403a882ff2ab59
Toms Dejus <Toms.Dejus@edu.rtu.lv>
2023-03-20 01:27:45 +0200

    Final

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 # python3
 
 def parallel_processing(n, m, data):
-    output = []
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
+    output = []
+    threads =[0]*n
+
+    for i in range(m):
+        min = min(threads)
+        thread = threads.index(min)
+
+        output.append((thread, min))
+        if i < len(data):
+            threads[thread]+=data[i]
 
     return output
+
 
 def main():
     # TODO: create input from keyboard
@@ -13,18 +23,19 @@ def main():
     # first line - n and m
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
+    n = int(input())
+    m = int(input())
 
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
+    data=list(map(int,input().split()))
 
     # TODO: create the function
     result = parallel_processing(n,m,data)
     
     # TODO: print out the results, each pair in it's own line
-
+    for pav_index, sak_laiks in result:
+        print(pav_index, sak_laiks)
 
 
 if __name__ == "__main__":
